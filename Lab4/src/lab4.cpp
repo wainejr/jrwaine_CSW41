49ca780ee951b2bd1fdd584d25cd64b2c3650109
Waine Oliveira Jr <waine@aerosim.io>
2022-09-17 00:02:41 -0300

    Corrected project configs
    - Now C is used and removed unnecessary file

--- Lab4/src/lab4.cpp
+++ Lab4/src/lab4.cpp
@@ -4,15 +4,6 @@
 
 #include "defines.h"
 
-#include "driverlib/gpio.h"
-#include "driverlib/interrupt.h"
-#include "driverlib/sysctl.h"
-#include "driverlib/systick.h"
-#include "driverlib/timer.h"
-#include "inc/hw_ints.h"
-#include "inc/hw_memmap.h"
-#include "inc/hw_types.h"
-
 int main()
 {
   return 0;
--- Lab4/src/startup_ewarm.c
+++ Lab4/src/startup_ewarm.c
@@ -27,17 +27,6 @@
 #include "inc/hw_nvic.h"
 #include "inc/hw_types.h"
 
-
-#include "driverlib/gpio.h"
-#include "driverlib/interrupt.h"
-#include "driverlib/sysctl.h"
-#include "driverlib/systick.h"
-#include "driverlib/timer.h"
-#include "inc/hw_ints.h"
-#include "inc/hw_memmap.h"
-#include "inc/hw_types.h"
-
-
 //*****************************************************************************
 //
 // Enable the IAR extensions for this source file.
